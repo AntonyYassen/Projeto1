c18ea72a15be1a4a000df30f80db958324c69ac3
GilmerQm <gilmer.qm@hotmail.com>
2025-02-18 20:16:54 -0500

    prueba commit

--- sistema/Model/Aluguel.cs
+++ sistema/Model/Aluguel.cs
@@ -1,23 +1,31 @@
-namespace SistemaAluguelVeiculos {
-    public class Aluguel {
+namespace SistemaAluguelVeiculos
+{
+    /// <summary>
+    /// prueba
+    /// </summary>
+    public class Aluguel
+    {
         public Cliente Cliente { get; set; }
         public Veiculo Veiculo { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
 
-        public Aluguel(Cliente cliente, Veiculo veiculo, DateTime dataInicio, DateTime dataFim) {
+        public Aluguel(Cliente cliente, Veiculo veiculo, DateTime dataInicio, DateTime dataFim)
+        {
             Cliente = cliente;
             Veiculo = veiculo;
             DataInicio = dataInicio;
             DataFim = dataFim;
         }
 
-        public double CalcularValorTotal() {
+        public double CalcularValorTotal()
+        {
             TimeSpan duracao = DataFim - DataInicio;
             return duracao.Days * Veiculo.PrecoPorDia;
         }
 
-        public void ExibirResumoAluguel() {
+        public void ExibirResumoAluguel()
+        {
             Console.WriteLine($"Cliente: {Cliente.Nome}");
             Console.WriteLine($"Veículo: {Veiculo.Modelo}");
             Console.WriteLine($"Período: {DataInicio.ToShortDateString()} a {DataFim.ToShortDateString()}");
